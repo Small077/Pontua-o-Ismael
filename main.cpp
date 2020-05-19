9f542898e7819ec5674183e99802a89c9bf2cdd5
Ismael Mendes <ismaelmendes.isma@gmail.com>
2020-05-19 08:31:09 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "SISTEMA DE PERFIL DE CLIENTE" <<endl;
-    cout << "--------------------------------" <<endl;
-    cout << "INFORME OS DADOS DO ULTIMO ANO" <<endl;
+    char D,C,B;
+    double tm;
+    int qc,pa,svc,si,sfp;
+
+    cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
     cout << endl;
     cout << "Quantas compras o cliente fez no ultimo ano? " << endl;
+    cin >> qc;
     cout << "Qual o ticket medio? " << endl;
+    cin >> tm;
     cout << endl;
     cout << "Quantas vezes o cliente atrasou o pagamento? " << endl;
     cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? " << endl;
