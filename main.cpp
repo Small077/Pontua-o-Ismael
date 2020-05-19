a62a988008dc93bba0a66645eb2e243c1b31aa4e
Ismael Mendes <ismaelmendes.isma@gmail.com>
2020-05-19 09:10:51 -0300

    Score de volume de compras

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    char D,C,B;
+    char tc;
     double tm;
     int qc,pa,svc,si,sfp;
 
@@ -16,11 +16,26 @@ int main()
     cin >> qc;
     cout << "Qual o ticket medio? " << endl;
     cin >> tm;
+    if (qc<=0){
+        svc=0;
+    }
+    else if (qc<=2 && tm<=3000){
+        svc=20;
+    }
+    else if (qc>2 && tm<=3000){
+        svc=40;
+    }
+    else if (tm>3000){
+        svc=60;
+    }
     cout << endl;
     cout << "Quantas vezes o cliente atrasou o pagamento? " << endl;
+    cin >> pa;
     cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? " << endl;
+    cin >> tc;
     cout << endl;
     cout << "Score de volume de compras = " << endl;
+    cout << svc;
     cout << endl;
     cout << "Score de inadimplencia = " << endl;
     cout << "Score de forma de pagamento = " << endl;
